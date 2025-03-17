b8bac72556f60a283d1d2115e1c37c0c1c148beb
Honour Ajagbe <jacobhonour9@gmail.com>
2025-03-17 11:17:58 +0100

    Make Light mode and Dark mode toggle

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 import {
   FaCircleUser,
@@ -9,6 +9,7 @@ import {
   FaAngleUp,
   FaCertificate,
   FaMoon,
+  FaSun,
   FaGraduationCap,
 } from "react-icons/fa6";
 import { BiSolidBook, BiSolidDoorOpen } from "react-icons/bi";
@@ -18,9 +19,38 @@ import logo from "../assets/valuemax-logo.png";
 
 const Admin = () => {
   const [isCertificationOpen, setIsCertificationOpen] = useState(false);
+  const [darkMode, setDarkMode] = useState(
+    localStorage.getItem("theme") === "dark"
+  );
+
+  useEffect(() => {
+    if (darkMode) {
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("theme", "darkMode");
+    } else {
+      document.documentElement.classList.remove("dark");
+      localStorage.setItem("theme", "light");
+    }
+    console.log("It works");
+  }, [darkMode]);
+
+  const dashboardBtnCss =
+    "flex flex-col justify-center items-center min-w-[280px] w-full";
+  const dashboardLinkCss =
+    "bg-white rounded-sm min-w-[280px] text-black p-2 mr-4 mt-2 grow";
+  const navLinkActiveCss =
+    "flex pt-3 justify-items-end mr-6 border-b border-yellow-200 text-cyan-400 rounded-lg pl-2 pr-2";
+  const navLinkNotActiveCss =
+    "flex pt-3 justify-items-end mr-6 hover:border-b hover:border-yellow-200 hover:text-cyan-400 rounded-lg pl-2 pr-2";
+  const navLinkBtnCss =
+    "bg-transparent flex justify-between items-center p-0 mt-2 mr-6 hover:border-b hover:border-yellow-200 hover:text-cyan-400 rounded-lg pl-2 pr-2 hide-outline";
+  const btnNavLinkActiveCss =
+    "pl-9 text-left pt-3 mr-6 border-b border-yellow-200 text-cyan-400 rounded-lg pl-2 pr-2";
+  const btnNavLinkNotActiveCss =
+    "pl-9 text-left pt-3 mr-6 hover:border-b hover:border-yellow-200 hover:text-cyan-400 rounded-lg pl-2 pr-2";
 
   return (
-    <div className="flex flex-row bg-blue-700">
+    <div className="flex flex-row bg-blue-700 dark:bg-gray-700">
       <aside className="min-w-[260px] md:w-[260px] h-screen">
         <div className="border-r border-b border-slate-400 p-4">
           <img src={logo} alt="valuemax-logo.png" />
@@ -34,9 +64,7 @@ const Admin = () => {
             to="/admin"
             end="true"
             className={({ isActive }) =>
-              isActive
-                ? "flex pt-3 justify-items-end mr-6 border-b border-yellow-200 text-cyan-400 rounded-lg pl-2 pr-2"
-                : "flex pt-3 justify-items-end mr-6 hover:border-b hover:border-yellow-200 hover:text-cyan-400 rounded-lg pl-2 pr-2"
+              isActive ? navLinkActiveCss : navLinkNotActiveCss
             }
           >
             <FaHouse size={28} className="pr-2" />
@@ -46,9 +74,7 @@ const Admin = () => {
             to="/"
             end="true"
             className={({ isActive }) =>
-              isActive
-                ? "flex pt-3 justify-items-end mr-6 border-b border-yellow-200 text-cyan-400 rounded-lg pl-2 pr-2"
-                : "flex pt-3 justify-items-end mr-6 hover:border-b hover:border-yellow-200 hover:text-cyan-400 rounded-lg pl-2 pr-2"
+              isActive ? navLinkActiveCss : navLinkNotActiveCss
             }
           >
             <FaBook size={25} className="pr-2" />
@@ -58,9 +84,7 @@ const Admin = () => {
             to="/"
             end="true"
             className={({ isActive }) =>
-              isActive
-                ? "flex pt-3 justify-items-center mr-6 border-b border-yellow-200 text-cyan-400 rounded-lg pl-2 pr-2"
-                : "flex pt-3 justify-items-center mr-6 hover:border-b hover:border-yellow-200 hover:text-cyan-400 rounded-lg pl-2 pr-2"
+              isActive ? navLinkActiveCss : navLinkNotActiveCss
             }
           >
             <FaUser size={25} className="pr-2" />
@@ -70,9 +94,7 @@ const Admin = () => {
             to="/"
             end="true"
             className={({ isActive }) =>
-              isActive
-                ? "flex pt-3 justify-items-center mr-6 border-b border-yellow-200 text-cyan-400 rounded-lg pl-2 pr-2"
-                : "flex pt-3 justify-items-center mr-6 hover:border-b hover:border-yellow-200 hover:text-cyan-400 rounded-lg pl-2 pr-2"
+              isActive ? navLinkActiveCss : navLinkNotActiveCss
             }
           >
             <FaUserTie size={25} className="pr-2" />
@@ -81,10 +103,9 @@ const Admin = () => {
           <button
             to="/"
             end="true"
-            className="bg-transparent flex justify-between items-center p-0 mt-2 mr-6 hover:border-b hover:border-yellow-200 hover:text-cyan-400 rounded-lg pl-2 pr-2 hide-outline"
+            className={navLinkBtnCss}
             onClick={() => {
               setIsCertificationOpen((prevState) => !prevState);
-              console.log("It worked");
             }}
           >
             <div className="flex items-center">
@@ -101,9 +122,7 @@ const Admin = () => {
               <NavLink
                 to="/"
                 className={({ isActive }) =>
-                  isActive
-                    ? "pl-9 text-left pt-3 mr-6 border-b border-yellow-200 text-cyan-400 rounded-lg pl-2 pr-2"
-                    : "pl-9 text-left pt-3 mr-6 hover:border-b hover:border-yellow-200 hover:text-cyan-400 rounded-lg pl-2 pr-2"
+                  isActive ? btnNavLinkActiveCss : btnNavLinkNotActiveCss
                 }
               >
                 Create Certificate
@@ -111,9 +130,7 @@ const Admin = () => {
               <NavLink
                 to="/"
                 className={({ isActive }) =>
-                  isActive
-                    ? "pl-9 text-left pt-3 mr-6 border-b border-yellow-200 text-cyan-400 rounded-lg pl-2 pr-2"
-                    : "pl-9 text-left pt-3 mr-6 hover:border-b hover:border-yellow-200 hover:text-cyan-400 rounded-lg pl-2 pr-2"
+                  isActive ? btnNavLinkActiveCss : btnNavLinkNotActiveCss
                 }
               >
                 Manage Certificate
@@ -123,60 +140,50 @@ const Admin = () => {
         </nav>
       </aside>
 
-      <main className="flex bg-slate-400 w-[2000px] flex-col">
+      <main className="flex bg-blue-600 w-[2000px] flex-col">
         <div className="bg-blue-700 h-[69px] w-full flex flex-row-reverse">
           <button className="w-[100px] bg-white flex rounded-sm justify-between items-center px-2 py-1 my-4 mr-4">
             <BiSolidDoorOpen size={20} />
             Log Out
           </button>
-          <button className="mr-6 text-white">
-            <FaMoon size={20} />
+          <button
+            className="mr-6 text-white"
+            onClick={() => {
+              setDarkMode((prevState) => !prevState);
+            }}
+          >
+            {darkMode ? <FaMoon size={20} /> : <FaSun size={20} />}
           </button>
         </div>
         <div className="m-6 flex flex-col items-start">
           <h2 className="text-4xl mb-4 text-white">Dashboard</h2>
           <div className="flex flex-row flex-wrap">
-            <Link
-              to="/"
-              className="bg-white rounded-sm min-w-[280px] text-black p-2 mr-4 mt-2 grow"
-            >
-              <button className="flex flex-col justify-center items-center w-full">
+            <Link to="/" className={dashboardLinkCss}>
+              <button className={dashboardBtnCss}>
                 <GoPerson size={70} />
                 Student Management
               </button>
             </Link>
-            <Link
-              to="/"
-              className="bg-white rounded-sm min-w-[280px] text-black p-2 mr-4 mt-2 grow"
-            >
-              <button className="flex flex-col justify-center items-center w-full">
+            <Link to="/" className={dashboardLinkCss}>
+              <button className={dashboardBtnCss}>
                 <BiSolidBook size={70} />
                 Course Management
               </button>
             </Link>
-            <Link
-              to="/"
-              className="bg-white rounded-sm text-black p-2 mr-4 mt-2 grow"
-            >
-              <button className="flex flex-col justify-center items-center min-w-[280px] w-full">
+            <Link to="/" className={dashboardLinkCss}>
+              <button className={dashboardBtnCss}>
                 <IoIosPeople size={70} />
                 Admin Management
               </button>
             </Link>
-            <Link
-              to="/"
-              className="bg-white rounded-sm text-black p-2 mr-4 mt-2 grow"
-            >
-              <button className="flex flex-col justify-center items-center min-w-[280px] w-full">
+            <Link to="/" className={dashboardLinkCss}>
+              <button className={dashboardBtnCss}>
                 <IoMdCloudUpload size={70} />
                 Upload Certificate
               </button>
             </Link>
-            <Link
-              to="/"
-              className="bg-white rounded-sm text-black p-2 mr-4 mt-2 grow"
-            >
-              <button className="flex flex-col justify-center items-center min-w-[280px] w-full">
+            <Link to="/" className={dashboardLinkCss}>
+              <button className={dashboardBtnCss}>
                 <FaGraduationCap size={70} />
                 Manage Certificate
               </button>
