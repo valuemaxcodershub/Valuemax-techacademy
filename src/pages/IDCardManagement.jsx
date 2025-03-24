fc068cb04afcac835b09782a2ce5a29fc36dce55
Honour Ajagbe <jacobhonour9@gmail.com>
2025-03-24 14:33:06 +0100

    create initial certificate management page layout

---
+++ src/pages/IDCardManagement.jsx
@@ -0,0 +1,105 @@
+import React, { useEffect, useState } from "react";
+import Sidebar from "../components/Sidebar";
+import students from "../students.json"
+import Header from "../components/Header";
+import SearchBar from "../components/SearchBar";
+
+const IDCardManagement = () => {
+  const [darkMode, setDarkMode] = useState(
+    localStorage.getItem("theme") === "darkMode"
+  );
+  const [filteredData, setFilteredData] = useState(students);
+
+  const handleSearch = (searchTerm) => {
+    if (!searchTerm) {
+      setFilteredData(students);
+    } else {
+      const filtered = students.filter(
+        (item) =>
+          item.Name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+          item.StudentID.toString().includes(searchTerm.toLowerCase()) ||
+          item.Course.toLowerCase().includes(searchTerm.toLowerCase()) ||
+          item.StartDate.toString().includes(searchTerm.toLowerCase())
+      );
+      setFilteredData(filtered);
+    }
+  };
+
+  useEffect(() => {
+    if (darkMode) {
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("theme", "darkMode");
+    } else {
+      document.documentElement.classList.remove("dark");
+      localStorage.setItem("theme", "light");
+    }
+    console.log("It works");
+  }, [darkMode]);
+
+  return (
+    <div
+      className={
+        darkMode
+          ? "bg-slate-800 flex flex-col grow"
+          : "bg-blue-700 flex flex-col grow"
+      }
+    >
+      <Header setDarkMode={setDarkMode} darkMode={darkMode} />
+      <div className="flex min-h-screen">
+        <div className="flex justify-between w-full">
+          <Sidebar mode={darkMode} />
+          <main
+            className={
+              darkMode
+                ? "bg-slate-700 text-white grow p-4 lg:p-6"
+                : "bg-blue-600 text-white grow p-4 lg:p-6"
+            }
+          >
+            <SearchBar onSearch={handleSearch} />
+            <table className="w-full mt-4">
+              <thead>
+                <tr className={darkMode ? "bg-black" : "bg-blue-900"}>
+                  <th className="p-2 text-left">Name</th>
+                  <th className="p-2 text-left">Student ID</th>
+                  <th className="p-2 text-left">Course</th>
+                  <th className="p-2 text-left">StartDate</th>
+                  <th className="p-2 text-left">Download</th>
+                </tr>
+              </thead>
+              <tbody>
+                {filteredData.map((item, index) => (
+                  <tr
+                    key={index}
+                    className={
+                      index % 2 === 0
+                        ? darkMode
+                          ? "bg-gray-800"
+                          : "bg-blue-700"
+                        : darkMode
+                        ? "bg-gray-900"
+                        : "bg-blue-800"
+                    }
+                  >
+                    <td className="p-2 text-left">{item.Name}</td>
+                    <td className="p-2 text-left">{item.StudentID}</td>
+                    <td className="p-2 text-left">{item.Course}</td>
+                    <td className="p-2 text-left">{item.StartDate}</td>
+                    <td className="p-2 text-left">
+                      <a href={item.IDCardDownloadLink} target="_blank">
+                        <button className="py-1 px-3 bg-white rounded-md text-black">
+                          Click
+                        </button>
+                      </a>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </main>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default IDCardManagement;
