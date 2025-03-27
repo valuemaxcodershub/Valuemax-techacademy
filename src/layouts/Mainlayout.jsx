1573c3799173984aac60ba21dd6021c853f216a7
olufemzy <oneholufemi@gmail.com>
2025-03-27 12:08:54 +0000

    removed redundances

--- src/layouts/Mainlayout.jsx
+++ src/layouts/Mainlayout.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Outlet } from "react-router-dom";
-import Header from '../components/header';
 import Footer from '../components/Footer';
+import Header from '../components/Header';
 
 
 const Mainlayout = () => {
